d3d34bd0695b569946b84e0dfdb1fcc0d4c5d9c1
doobin <doobin42@gmail.com>
2018-11-20 16:46:01 -0500

    Updates

---
+++ precache-manifest.ca344027ff1ce90f8eb8d24ad9b3d66a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "676d99cc1e20c7688a28",
+    "url": "/First-React-App/static/js/runtime~main.676d99cc.js"
+  },
+  {
+    "revision": "6aede32d6ee6f731dc74",
+    "url": "/First-React-App/static/js/main.6aede32d.chunk.js"
+  },
+  {
+    "revision": "3f0b22993c54ddcf39f8",
+    "url": "/First-React-App/static/js/1.3f0b2299.chunk.js"
+  },
+  {
+    "revision": "6aede32d6ee6f731dc74",
+    "url": "/First-React-App/static/css/main.f77595a9.chunk.css"
+  },
+  {
+    "revision": "c6f66723694546582e8ec6dec4e15a1b",
+    "url": "/First-React-App/index.html"
+  }
+];
