5c3bb1d8f82ba7e7969e38d48326b5833b393033
doobin <doobin42@gmail.com>
2018-11-20 16:46:27 -0500

    Initial Deploy

--- src/Family.js
+++ src/Family.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react'
 import Member from './Member.js'
+import './Family.scss'
 
 class Family extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
+      liked: false,
       hidden: false
     }
 
     // This binding is necessary to make `this` work in the callback
+    this.toggleLiked = this.toggleLiked.bind(this)
     this.toggleHidden = this.toggleHidden.bind(this)
   }
+
+  toggleLiked() {
+      this.setState({
+        liked: !this.state.liked
+      })
+    }
 
   toggleHidden() {
     this.setState({
@@ -21,10 +30,14 @@ class Family extends Component {
 
   render () {
     const { name, people } = this.props
+    const { liked, hidden } = this.state
     return (
       <div>
-        <h4>Family Name: {name}</h4>
-        <ul>
+        <h4 className={liked ? 'liked' : ''}> Family Name: {name}</h4>
+        <button onClick={this.toggleLiked}>{liked ? 'Unlike' : 'Like'}</button>
+        <p>Members: </p>
+        <button onClick={this.toggleHidden}>{hidden ? 'Show' : 'Hide'}</button>
+        <ul className={hidden ? 'hidden' : ''}>
           {people.map(member => (
             <Member
               key={member.name}
