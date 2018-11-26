583eed76aca8d2da48f520c67efc9b135073ba63
doobin <doobin42@gmail.com>
2018-11-26 12:27:50 -0500

    /families renders message

--- src/Family.js
+++ src/Family.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react'
-import gameOfThronesHouses from './GOTHousesData.js'
 import './Family.scss'
 
 class Family extends Component {
   render () {
     return (
       <div>
-        <h4> Family Name: </h4>
-        <ul>
-        {gameOfThronesHouses.map(house => (
-          <Family
-            key={house.id}
-            name={house.name}
-          />
-        ))}
-        </ul>
+        <p>Its connected</p>
       </div>
     )
   }
